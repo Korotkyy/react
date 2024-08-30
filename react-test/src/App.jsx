d3756d32e1d59f3f44853defcdb75aea4949d3f4
Korotkyy <118772636+Korotkyy@users.noreply.github.com>
2024-08-30 18:00:07 +0200

    save package-json

--- react-test/src/App.jsx
+++ react-test/src/App.jsx
@@ -1,14 +1,21 @@
 import Footer from "./components/Footer/footer";
 import Header from "./components/Header/Header";
+import Message from "./components/Message/message";
 
 
 
 const App = () => {
+  const isOnline = true;
   return (
-    <div>
+
+      <>
       <Header />
+      {isOnline && <h2>Welcome</h2>}
+      <p>Super forum</p>
+      <Message author = 'Andru' message = 'куплю айфон 15'/>
+      <Message author = 'Anna' message = 'I love you'/>
       <Footer />
-    </div>
+      </>
   );
 };
 export default App;
---
+++ react-test/src/components/Message/message.jsx
@@ -0,0 +1,10 @@
+const Message = ({message, author}) => {
+  return (
+    <div>
+      <h3>{message}</h3>
+      <p>Author: {author}</p>
+    </div>
+  )
+}
+
+export default Message
