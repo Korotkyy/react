208e83a4ab41b95f6e01613e99169de629e4361e
Korotkyy <118772636+Korotkyy@users.noreply.github.com>
2024-08-30 14:32:39 +0200

    create first components

---
+++ react-test/src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer/footer";
+import Header from "./components/Header/Header";
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+export default App;
---
+++ react-test/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+      <header>
+        <div>LOGO</div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Login</li>
+        </ul>
+        <hr />
+      </header>
+    );
+  };
+  export default Header
